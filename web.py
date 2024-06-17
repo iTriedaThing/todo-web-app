03c8bfcb6f6af62f7517269a2cb830a090939758
Ben Malat <BenMalat@duck.com>
2024-06-16 20:22:49 -0400

    adding items to the checkbox

--- web.py
+++ web.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import functions
 
-todos=functions.read_todos()
+todos = functions.read_todos()
+
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
@@ -10,4 +17,8 @@ st.write("This app is to increase productivity and drive me crazy")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder='Enter a todo')
+st.text_input(label="the todo box", label_visibility='hidden',
+              placeholder='Enter a todo',
+              on_change=add_todo, key='new_todo')
+
+st.session_state
