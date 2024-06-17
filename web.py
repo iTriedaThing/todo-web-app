f48dca24850517e5f8b126b5a8eb53f830d782e0
Ben Malat <BenMalat@duck.com>
2024-06-16 20:42:25 -0400

    working web app, pre-deployment

--- web.py
+++ web.py
@@ -14,11 +14,13 @@ st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase productivity and drive me crazy")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="the todo box", label_visibility='hidden',
-              placeholder='Enter a todo',
-              on_change=add_todo, key='new_todo')
-
-st.session_state
+              placeholder='Enter a todo', on_change=add_todo, key='new_todo')
